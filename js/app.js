5591d84d37b26a28b112716d9eaf3301b1ee9a92
Diana Wallace <dianawallacedeveloper@gmail.com>
2016-05-01 21:16:40 +0000

    added new game function

--- js/app.js
+++ js/app.js
@@ -12,6 +12,17 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+    /* This is where my code starts */
+    
+    
+    
+    
+    
+    $('.new').on('click', function() {
+        window.location.reload(true);
+    });
+
+
 });
 
 
