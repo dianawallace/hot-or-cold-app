197cd04f043c98f25a078dcf4919c610ce5d506b
Diana Wallace <dianawallacedeveloper@gmail.com>
2016-05-02 05:38:37 +0000

    feedback, counter, display guesses

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-$(document).ready(function(){
+jQuery(document).ready(function(S){
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -14,12 +14,60 @@ $(document).ready(function(){
 
     /* This is where my code starts */
     
-    /* Generates a random number between one and a hundred */
+   
+    /* Display number of guesses */
+    
+    var count = 0;
+    var number;
+    
+    function incrementCounter() {
+        counter++;
+        $('#count').text(count);
+    };
+    
+     /* Generates a random number between one and a hundred */
     
     function randomNumber() {
-        Math.floor(Math.random()*100 + 1);
-        window.onload=randomNumber;
-    }
+        number = Math.floor((Math.random()*100) + 1);
+    };
+    
+    /* Display numbers guessed */
+    
+    $("#guessButton").click(function(e) {
+        e.preventDefault();
+        var guess = $("#userGuess").val();
+        var guessE1 = '<li>' + guess + '</li>';
+            $('#guessList').append(guessE1);
+            $('#userGuess').val('');
+            incrementCounter();
+            checkGuess(guess);
+    });
+    
+    
+    /* Hot or Cold */
+    
+    var diff; 
+    var guessInt = parseInt(guess);
+    
+        if (guess === number) {
+            $('#feedback').text('Correct');
+        } else if (guessInt > number) {
+            diff = guessInt - number;
+        } else if (guessInt < number) {
+            diff = number - guessInt;
+        }
+        
+        if (diff >= 1 && diff <= 10) {
+            $("#feedback").text('Very Hot!');
+        } else if (diff < 10 && diff <= 20) {
+            $("#feedback").text('Hot');
+        } else if (diff < 20 && diff <= 30) {
+            $("#feedback").text('Warm');
+        } else if (diff < 30 && diff <= 50) {
+            $("#feedback").text('Cold');
+        } else (diff > 50) {
+            $("#feedback").text('Ice Cold');
+        }
     
     
     $('.new').on('click', function() {
